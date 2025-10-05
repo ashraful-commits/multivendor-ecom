569b7fccdcc623fb3289dce9b55a637988ab1f3b
Md ashraful alam <ashrafulalam10000@gmail.com>
2025-10-05 16:29:00 +0600

    update prisma

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,24 @@
 import { PrismaClient } from "@prisma/client";
 
-declare const globalThis: any;
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-const db = globalThis.prisma || new PrismaClient();
+const prismaClientSingleton = () => {
+  const prisma = new PrismaClient({
+    log: ["error"],
+    errorFormat: "minimal",
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+  });
+
+  return prisma;
+};
+
+const db = globalThis.prisma ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prisma = db;
